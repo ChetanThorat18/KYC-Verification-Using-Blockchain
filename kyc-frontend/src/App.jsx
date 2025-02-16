8128db2fa4dba5f4d27b75d0044f4ff31b912e49
Chetan <chetan@Chetans-MacBook-Air.local>
2025-02-16 19:39:31 +0530

    Initial Setup

---
+++ kyc-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center text-blue-600">
+        Welcome to Blockchain KYC Verification
+      </h1>
+    </>
+  );
+}
+
+export default App;
